ed67766cdd636b67aa737d9911911e066e69458f
Khushi Patel <khushiwonder@gmail.com>
2026-01-24 01:10:56 +0530

    practing program

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args){
+        String fruits = "Apple";
+        System.out.println("I like "+fruits);
+        System.out.println("It's really good!");
+    }
+} 
