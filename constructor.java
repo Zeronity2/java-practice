5616fa38055310ba9ea92a763762c0d8374d76c5
Khushi Patel <khushiwonder@gmail.com>
2026-01-23 22:43:28 +0530

    Added java practice program

---
+++ constructor.java
@@ -0,0 +1,31 @@
+class Student{
+    int roll;
+    String name;
+    float marks;
+    void getValue(int r, String n, float m){
+        roll = r;
+        name = n;
+        marks = m;
+    }
+
+    void ShowValue(){
+        System.out.println("the name is:"+name);
+        System.out.println("the roll no. is:"+roll);
+        System.out.println("the marks are:"+marks);
+    }
+
+    void grace(){
+        if(marks > 80){
+            marks = marks + 3;
+        }
+    }
+}
+
+class TestStudent{
+    public static void main(String args[]){
+        Student Khushi = new Student();
+        Khushi.getValue(26,"Khushi",85);
+        Khushi.grace();
+        Khushi.ShowValue();
+    }
+}
