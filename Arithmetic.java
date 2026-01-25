b91233e985a9305e6f99ff99bd0ef48c9eca5bc9
Khushi Patel <khushiwonder@gmail.com>
2026-01-26 00:23:07 +0530

    arithmectic practice

---
+++ Arithmetic.java
@@ -0,0 +1,26 @@
+
+public class Arithmetic{
+    public static void main(string[] args){
+
+        //Arithmetic Operators
+        // int x = 10;
+        // int y = 2;
+        // int z;
+
+        //z = x + y;
+        //z = x - y;
+       //z = x * y;
+       //z = x / y;
+       //z = x % y;
+
+       //Increment and Decrement Operators
+
+      int x = 1;
+    //    x++;
+     x--;
+
+    // order of operations [P-E-M-D-A-S]
+
+       System.out.println(x);
+    }
+}
