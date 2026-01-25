9e21baa02b42f26c448ed06667ce24ea31db7f9e
Khushi Patel <khushiwonder@gmail.com>
2026-01-25 22:42:32 +0530

    o calculate the area of reactangle by taking user input

--- areaREC.java
+++ areaREC.java
@@ -2,15 +2,19 @@ import java.util.Scanner;
 
 public class areaREC{
     public static void main(String[] args){
+        double l = 0;
+        double b = 0;
+        double area = 0;
+        
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter length: ");
-        double l = scanner.nextDouble();
+        l = scanner.nextDouble();
 
         System.out.print("Enter breadth: ");
-        double b = scanner.nextDouble();
+        b = scanner.nextDouble();
 
-        double area = (l*b);
+        area = (l*b);
 
         System.out.println("Area of rectangle: "+area);
         scanner.close();
