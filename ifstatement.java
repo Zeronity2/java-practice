d8ba51d8e2eaf0b1a00de22f31be45246e55502a
Khushi Patel <khushiwonder@gmail.com>
2026-01-27 22:33:15 +0530

    if else program

--- ifstatement.java
+++ ifstatement.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;     
+
 public class ifstatement{
     public static void main(String[] args){
         // if statement = perform a block of code if its condition is true
-    int age = 70 ;
+    Scanner scanner = new Scanner(System.in);
+    
+    int age;
+    // String name;
+    // boolean isStudent;
 
+    System.out.print("Enter your age: ");
+    age = scanner.nextInt();
+    
+    // System.out.print("Enter your name: ");
+    // name = scanner.nextLine();
+
+    // System.out.print("Are you a student (true/false): ");
+    // isStudent = scanner.nextBoolean();
+
+// GROUP 1
+// if(name.isEmpty()){
+//     System.out.println("You didn't enter your name!");
+// }
+// else{
+//     System.out.println("Hello "+ name + "!");
+// }
+
+
+// GROUP 2
     if(age >= 18){
         System.out.println("You are an adult!");
     }
@@ -10,7 +35,7 @@ public class ifstatement{
         System.out.println("You are a senior!");
     }
     else if(age < 0){
-        System.out.println("You haven't been born yet!");
+        System.out.println("You haven't been born yet! ");
     }
     else if(age == 0){
         System.out.println("You are a baby!");
@@ -18,6 +43,16 @@ public class ifstatement{
     else{
         System.out.println("You are a child!");
     }
+
+// // GROUP 3
+//     if(isStudent){
+//         System.out.println("You are a student!");
+//     }
+//     else{
+//         System.out.println("You are not a student!");
+//     }
+
+    scanner.close();
 
     }
 }
