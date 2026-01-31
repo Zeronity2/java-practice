c81a0275c0820c5b6ac6f78970c937f45ae11e58
Khushi Patel <khushiwonder@gmail.com>
2026-01-31 13:29:14 +0530

    ternary operator concept

---
+++ ternaryOperator.java
@@ -0,0 +1,17 @@
+
+public class ternaryOperator{
+    public static void main(String[] args){
+
+        // ternary operator ? = return 1 of value if condition is true
+        
+        // variable = (condition) ? ifTrue : ifFalse;
+
+        int number = 10;
+
+        String evenOrodd = (number % 2 == 0) ? "Even" : "Odd";
+
+        System.out.println(evenOrodd);
+
+
+    }
+}
