e524c504bba31f9f9d5eec1ebac3eae53065db1d
Khushi Patel <khushiwonder@gmail.com>
2026-02-01 14:56:23 +0530

    Do while program

---
+++ doWhile.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class doWhile{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+         int age = 0;
+
+         do{
+            System.out.println("Your age can't be negative!");
+            System.out.print("Enter your age: ");
+            age = sc.nextInt();
+         }
+         while(age < 0);
+            System.out.println("You are " +age+ " years old");
+    
+            sc.close();
+        
+    }
+}
