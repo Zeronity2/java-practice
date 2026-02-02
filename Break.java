59532d24c0f210176218ff933759d37aaf997664
Khushi Patel <khushiwonder@gmail.com>
2026-02-02 19:17:40 +0530

    Break and Continue Statement

---
+++ Break.java
@@ -0,0 +1,24 @@
+
+public class Break{
+    public static void main(String[] args){
+
+        //break = break out of a loop (STOP)
+        //continue = skip current iteration of a loop (SKIP)
+
+        // for(int i = 0; i < 10; i++){
+        //     if(i==5){
+        //         break;
+        //     }
+        //     System.out.print(i + " ");
+
+        for(int i = 0; i < 10; i++){
+            
+            if(i == 5){
+                continue;
+            }
+
+            System.out.print(i +" ");
+        }
+
+        }
+    }
