84dccf1e41e001638386bdde64a012a26d5afe34
Khushi Patel <khushiwonder@gmail.com>
2026-02-03 10:32:19 +0530

    nested loop program

---
+++ nestedLoop.java
@@ -0,0 +1,13 @@
+
+public class nestedLoop{
+    public static void main(String[] args){
+
+        // nested loop = A loop inside another loop
+        //  
+
+        for()
+
+            }
+        }
+    }
+}
