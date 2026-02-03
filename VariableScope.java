118dc67d13802cd35d0fa39851c90ed8fcc89022
Khushi Patel <khushiwonder@gmail.com>
2026-02-03 22:41:18 +0530

    variable scope local and class

---
+++ VariableScope.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class VariableScope{
+
+    static int x = 3;//class
+
+    public static void main(String[] args){
+
+        // variable scope = where a variable can be accessed (local or class)       
+
+        Scanner sc = new Scanner(System.in);
+        
+        int x = sc.nextInt();//local
+
+        dosomething();
+
+    }
+    static void dosomething(){
+
+        int x = 2;//Local
+
+        System.out.println(x);
+    }
+
+}
