8ee02339b0569031c9fee36ca6ee6a811e20121f
Khushi Patel <khushiwonder@gmail.com>
2026-02-05 23:23:25 +0530

    searching an array

--- SearchArray.java
+++ SearchArray.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
 
 public class SearchArray{
     public static void main(String[] args){
 
-
+        Scanner sc = new Scanner(System.in);
         
+        int[] numbers = {1, 9, 2, 8, 5, 4};
+        String[] fruits = {"apple", "banana", "orange"};
+        boolean isFound = false;
+        String target;
+
+        System.out.print("Enter a fruit to serach for: ");
+        target = sc.nextLine();
+
+        for(int i = 0; i<fruits.length; i++){
+            if (target.equals(fruits[i])){
+                System.out.println("Element found at "+i);
+                isFound = true;
+                break;
+            }
+            if(!isFound){
+                System.out.println("Element not found in the array!");
+            }
+        }
+        sc.close();
     }
 }
