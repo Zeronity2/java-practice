e1760b33482d5266e4dcaad95358ac33aebf94cc
Khushi Patel <khushiwonder@gmail.com>
2026-02-05 23:49:35 +0530

    2D array program

--- TwoDarray.java
+++ TwoDarray.java
@@ -1,7 +1,27 @@
+import java.util.*;
 
 public class TwoDarray{
     public static void main(String[] args){
 
-        
+     Scanner sc = new Scanner(System.in);
+
+     int[][] arr = new int[2][2];
+
+     System.out.print("Enter the elements: ");
+
+     for(int i =0; i<arr.length; i++){
+        for(int j = 0; j<arr[i].length; j++){
+            arr[i][j] = sc.nextInt();
+        }
+     }
+
+
+    System.out.print("Matrix: ");
+    for(int i = 0; i<arr.length; i++){
+        for(int j=0; j<arr[i].length; j++){
+            System.out.print(arr[i][j] +" ");
+        }
+        System.out.println();
+     }
     }
 }
