c0386d6a52f7a55aaf2a65bf2e9134198fe0e5cd
Khushi Patel <khushiwonder@gmail.com>
2026-02-06 09:38:11 +0530

    multiplication of multidimensional array

--- Multiply2Darray.java
+++ Multiply2Darray.java
@@ -23,7 +23,7 @@ public class Multiply2Darray{
          for(int i=0; i<arr1.length ; i++){
             for(int j =0; j<arr2[0].length; j++){
                 for(int k=0; k<arr1[0].length; k++){
-                    arr3[i][j] += arr1[i][k] + arr2[j][k];
+                    arr3[i][j] += arr1[i][k] * arr2[j][k];
                 }
             }
         }
