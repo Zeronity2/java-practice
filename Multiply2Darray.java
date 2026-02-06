d85b4d55c09a942611d6c5264170935d0f68c0dd
Khushi Patel <khushiwonder@gmail.com>
2026-02-06 10:35:06 +0530

    Multiplication of multidimensional array

--- Multiply2Darray.java
+++ Multiply2Darray.java
@@ -7,13 +7,15 @@ public class Multiply2Darray{
         int[][] arr1 = new int[2][2];
         int[][] arr2 = new int[2][2];
         int[][] arr3 = new int[2][2];
-
+        
+        System.out.print("Enter first matrix: ");
         for(int i=0; i<arr1.length ; i++){
             for(int j =0; j<arr1[i].length; j++){
                 arr1[i][j] = sc.nextInt();
             }
         }
-
+        
+        System.out.print("Enter second matrix: ");
          for(int i=0; i<arr2.length ; i++){
             for(int j =0; j<arr2[i].length; j++){
                 arr2[i][j] = sc.nextInt();
@@ -22,12 +24,13 @@ public class Multiply2Darray{
 
          for(int i=0; i<arr1.length ; i++){
             for(int j =0; j<arr2[0].length; j++){
+                arr3[i][j]=0;
                 for(int k=0; k<arr1[0].length; k++){
                     arr3[i][j] += arr1[i][k] * arr2[j][k];
                 }
             }
         }
-
+        System.out.print("Multiplication of matrix: ");
         for(int[] i : arr3){
             for(int result : i){
                 System.out.print(result +" ");
