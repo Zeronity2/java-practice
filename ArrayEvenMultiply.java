42eccd8afafa7c78ea956e499eaacd35fc90b740
Khushi Patel <khushiwonder@gmail.com>
2026-02-12 20:27:33 +0530

    array practice

---
+++ ArrayEvenMultiply.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ArrayEvenMultiply{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int product = 1;
+
+        for(int i =0; i<n ; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i=1; i<n; i=i+2){
+            product = product * arr[i];
+        }
+        System.out.println(product);
+    }
+}
