749e3bbf46a8cb9fed5cadb05601e5f6e5dc0cf0
Khushi Patel <khushiwonder@gmail.com>
2026-02-12 11:10:19 +0530

    loop practicing

--- pattern3.java
+++ pattern3.java
@@ -1,8 +1,13 @@
+import java.util.*;
+
 public class pattern3{
     public static void main(String[] args){
-
-        for(int i = 1; i<=4; i++){
-            for(int j = 4; j>i; j--){
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter no.: ");
+      int n = sc.nextInt();
+      
+        for(int i = 1; i<=n; i++){
+            for(int j = n; j>i; j--){
                 System.out.print(" ");
             }
             for(int k = 1; k<=(2*i)-1; k++){
