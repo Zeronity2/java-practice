7905cd88dd2b93f22b0912be762a904927cfadc1
Khushi Patel <khushiwonder@gmail.com>
2026-02-19 21:15:50 +0530

    string reverse program

--- StringRev.java
+++ StringRev.java
@@ -1,18 +1,16 @@
 import java.util.Scanner;
 
 public class StringRev{
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the string: ");
+        String s = sc.nextLine();
 
-        System.out.print("Enter a string: ");
-        String str = sc.nextLine();
-
-        String reversed = "";
-
-        for (int i = str.length() - 1; i >= 0; i--) {
-            reversed += str.charAt(i);
+        String rev = " ";
+        for(int i = s.length()-1; i>=0; i--){
+            rev = rev + s.charAt(i);
         }
-
-        System.out.println("Reversed string: " + reversed);
+        System.out.println("Reverse string: "+rev);
     }
 }
