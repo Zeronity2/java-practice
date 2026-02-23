d3a824cdf219a0f1fe6a0a7fea089b594e1bb0d3
Khushi Patel <khushiwonder@gmail.com>
2026-02-23 22:04:37 +0530

    Math class

--- mathclass.java
+++ mathclass.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 
 public class mathclass{
     public static void main(String[] args){
 
         // System.out.println(Math.PI);
         // System.out.println(Math.E);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a: ");
+        int a = sc.nextInt();
+        System.out.print("Enter b: ");
+        int b = sc.nextInt();
 
         double result;
 
@@ -14,7 +22,8 @@ public class mathclass{
         // result = Math.ceil(3.14);
         // result = Math.floor(3.99);
         // result = Math.max(10, 20);
-        result = Math.min(10, 20);
+
+        result = Math.min(a, b);
 
         System.out.println(result);
     }
