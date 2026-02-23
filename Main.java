a26e8ca04e80b1e71692859b9f14167ca6affbb4
Khushi Patel <khushiwonder@gmail.com>
2026-02-23 22:07:05 +0530

    main basic program

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
-        String fruits = "Apple";
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter String: ");
+        String fruits = sc.nextLine();
+        
         System.out.println("I like "+fruits);
         System.out.println("It's really good!");
     }
