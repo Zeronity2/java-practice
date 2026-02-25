713b9cc26b03d586ef6bb91094c7c190981ab754
Khushi Patel <khushiwonder@gmail.com>
2026-02-25 23:01:54 +0530

    in array even multiply

--- ArrayEvenMultiply.java
+++ ArrayEvenMultiply.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+
 public class ArrayEvenMultiply{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
+        
+        System.out.print("Enter the number: ");
         int n = sc.nextInt();
         int[] arr = new int[n];
         int product = 1;
