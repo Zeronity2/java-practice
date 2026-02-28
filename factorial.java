0fd3cbf73006942655c8f65a2a383ab1a0e8ebd6
Khushi Patel <khushiwonder@gmail.com>
2026-02-28 20:05:30 +0530

    28 Feb commit

--- factorial.java
+++ factorial.java
@@ -13,7 +13,7 @@ public class factorial{
             fact = fact * i;
 
         }
-        System.out.println("Factorial of number: "+fact);
+        System.out.println("Factorial of the number: "+fact);
 
         sc.close();
     }
