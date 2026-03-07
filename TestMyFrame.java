0758d60f1385c2b7124a3ff57fd43a88dad99b95
Khushi Patel <khushiwonder@gmail.com>
2026-03-07 21:07:43 +0530

    basic frame java code

--- TestMyFrame.java
+++ TestMyFrame.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 class MyFrame extends JFrame implements ActionListener{
     TextField t1, t2, t3;
     Label l1, l2, l3;
-    Button b1, b2;
+    Button b1, b2, b3;
     int i1, i2, i3;
     String s1, s2, s3;
 
@@ -21,9 +21,11 @@ class MyFrame extends JFrame implements ActionListener{
 
       b1 = new Button("Add");
       b2 = new Button("Clear");
+      b3 = new Button("Subtract");
 
       b1.addActionListener(this);
       b2.addActionListener(this);
+      b3.addActionListener(this);
 
       Container con = getContentPane();
       con.setLayout(new FlowLayout());
@@ -34,6 +36,7 @@ class MyFrame extends JFrame implements ActionListener{
       con.add(l3);
       con.add(t3);
       con.add(b1);
+      con.add(b3);
       con.add(b2);
     }
 
@@ -49,7 +52,14 @@ class MyFrame extends JFrame implements ActionListener{
       t2.setText("");
       t3.setText("");
      }
+     if(ae.getSource() == b3){
+        i1 = Integer.parseInt(t1.getText());
+        i2 = Integer.parseInt(t2.getText());
+        i3 = i1-i2;
+        t3.setText(""+i3);
+     }
     }
+
 }
 
 class TestMyFrame{
