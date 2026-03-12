c737df483a3aa6ff81722f412c0ec422220eff8e
Khushi Patel <khushiwonder@gmail.com>
2026-03-12 10:49:21 +0530

    interface in java

---
+++ TestInterface.java
@@ -0,0 +1,22 @@
+interface Area{
+    public final float  PI = 3.14f;
+    double compute(double a, double b);
+}
+class Rec implements Area{
+   public double compute(double a , double b){
+        return (a * b);
+    }
+}
+class Circle implements Area{
+   public double compute(double a, double b){
+        return (PI * a * a);
+    }
+}
+class TestInterface{
+    public static void main(String[] args){
+        Rec obj = new Rec();
+        Circle obj2 = new Circle();
+        System.out.println("Area of Rectangle: "+obj.compute(2,3));
+        System.out.println("Area of Circle: "+obj2.compute(2,3));
+    }
+}
