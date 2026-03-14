6e8d1ad37e032e74bd1957295a23becac784ed68
Khushi Patel <khushiwonder@gmail.com>
2026-03-14 20:22:42 +0530

    use of super in inheritance

--- TestSuper.java
+++ TestSuper.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 class Vehicle {
-    int maxSpeed = 120;   
+    int maxSpeed = 120;  
+    int minSpeed = 40; 
 }
 
 class Car extends Vehicle {
@@ -13,11 +14,12 @@ class Car extends Vehicle {
 
     void display() {
         System.out.println("Maximum Speed from Base class (Vehicle): " + super.maxSpeed);
+        System.out.println("Minimun Speed from Base class (Vehicle): "+super.minSpeed);
         System.out.println("Maximum Speed from Subclass (Car): " + maxSpeed);
     }
 }
 
-public class Main {
+public class TestSuper {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
