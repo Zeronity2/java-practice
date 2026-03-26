4f987a3c38178cdcdb47042e0404223cb6d9accc
Khushi Patel <khushiwonder@gmail.com>
2026-03-26 21:04:46 +0530

    local class

--- TestAnony.java
+++ TestAnony.java
@@ -1,15 +1,26 @@
+@FunctionalInterface
 interface Area{
-    public final float PI = 3.14f;
-    public void calculate(double a, double b);
+    public Static final float PI = 3.14f;
+    public double calculate(double a, double b);
 }
 
 public class TestAnony{
     public static void main(String[] args){
-        Area rec = new Area();
-            public void calculate(double a, double b){
+        Area rec = new Area(){
+            public double calculate(double a, double b){
             return (a*b);
             }
+        };
+        Area circle = new Area(){
+            public double calculate(double a, double b){
+                return (PI*a*a);
+            }
+        };
+        // Area r = (a,b)->(a*b);
+        // Area c = (a,b)->(Area.PI*a*a);   //lembda syntax
+
              
         System.out.println("The area of rectangle: "+rec.calculate(5,8));
+        System.out.println("The area of circle: "+circle.calculate(1,1));
     }
 }
