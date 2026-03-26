63e8aabaccad8cfe8d2d2b68af773110e1e3afe8
Khushi Patel <khushiwonder@gmail.com>
2026-03-26 21:14:24 +0530

    use of area to compute area

--- TestLembda.java
+++ TestLembda.java
@@ -5,8 +5,8 @@ interface Area{
 }
 class TestLembda{
     public static void main(String[] args){
-        Area rec = (a,b)->(a*b);//lembda
-        Area circle = (a,b)->(Area.PI*a*a);
+        Area rec = (a,b)->(a*b);//lembda to calculate the area of rectangle
+        Area circle = (a,b)->(Area.PI*a*a);//area of circle
 
         System.out.println("The area of rectangle: "+rec.compute(3,4));
         System.out.println("The ares of circle: "+circle.compute(1,0));
