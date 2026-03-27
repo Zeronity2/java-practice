0ff37ff8c580392e2f8f72cc1ec692a5336a74ed
Khushi Patel <khushiwonder@gmail.com>
2026-03-28 00:00:46 +0530

    interface implements

--- TestInterface.java
+++ TestInterface.java
@@ -11,13 +11,19 @@ class Circle implements Area{
    public double compute(double a, double b){
         return (PI * a * a);
     }
-class Square
+class Square implements Area{
+    public double compute(double a, double b){
+        return (a*a);
+    }
+}
 }
 class TestInterface{
     public static void main(String[] args){
         Rec obj = new Rec();
         Circle obj2 = new Circle();
+        Square obj3 = new Square();
         System.out.println("Area of Rectangle: "+obj.compute(2,3));
         System.out.println("Area of Circle: "+obj2.compute(2,3));
+        System.out.println
     }
 }
